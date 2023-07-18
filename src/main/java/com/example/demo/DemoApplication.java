e1c4e5a3cf0d8b7f4fc013f74e8c1324739da5f8
wisdom <bsbpsj97@naver.com>
2023-07-18 23:34:47 +0900

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
+		System.out.println("branch test");
 		System.out.println("Test commit add5");
 		System.out.println("Test commit 메롱~~~");
 	}
