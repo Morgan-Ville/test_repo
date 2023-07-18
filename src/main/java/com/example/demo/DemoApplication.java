aa9bb42df394f4baf857b0a6866a9d17a4fa0079
wisdom <bsbpsj97@naver.com>
2023-07-18 22:50:30 +0900

    test commit2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 
 		System.out.println("Test commit add");
+		System.out.println("Test commit add2");
 	}
 
 }
